0984a3b79c1316cca2a9e1e6db906e8f76e35267
Chris C <chris.c.1221@gmail.com>
2015-08-18 15:47:23 -0400

    add align and master, need X, issue #1

---
+++ app/align_check.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#$ -S /bin/bash
+#$ -q abaqus.q
+#$ -l qname=abaqus.q
+#$ -cwd
+#$ -V
+#$ -l mf=192G
+#$ -j y
+#$ -o /home/hpc2862/logs/$JOB_NAME.txt
+
+cd /home/hpc2862/Scripts/impute
+
+CHR=$1
+REPO="/home/hpc2862/Scripts/impute"
+REF_DIR="/scratch/hpc2862/CAMH/jen/ALL.integrated_phase1_SHAPEIT_16-06-14.nomono"
+DATA_DIR="/scratch/hpc2862/CAMH/jen/NEAM"
+
+bin/shapeit -check -B ${DATA_DIR}/NEAM_QC_complete_150623_chr${CHR} -M ${REF_DIR}/genetic_map_chr${CHR}_combined_b37.txt --input-ref ALL.chr${CHR}.integrated_phase1_v3.20101123.snps_indels_svs.genotypes.nomono.haplotypes.gz ${REF_DIR}/ALL.chr${CHR}.integrated_phase1_v3.20101123.snps_indels_svs.genotypes.nomono.legend.gz ALL.integrated_phase1_v3.20101123.snps_indels_svs.genotypes.sample --output-log neam_alignment_chr${CHR}
