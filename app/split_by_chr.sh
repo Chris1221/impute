ab77151383790145d89704c6bd27115e400c5cbf
Chris C <chris.c.1221@gmail.com>
2015-08-18 15:11:15 -0400

    split and sub scripts

---
+++ app/split_by_chr.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#$ -S /bin/bash
+#$ -q abaqus.q
+#$ -l qname=abaqus.q
+#$ -cwd
+#$ -V
+#$ -l mf=192G
+#$ -j y
+#$ -o /home/hpc2862/logs/${JOB_NAME}.txt
+
+cd /scratch/hpc2862/CAMH/jen/NEAM
+
+/home/hpc2862/Programs/binary_executables/plink --bfile NEAM_QC_complete_150623 --chr $1 --make-bed --out NEAM_QC_complete_150623_chr${1}
