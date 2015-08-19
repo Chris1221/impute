68875435dce1e1f2173fcb61215e7a970f4a0f26
Chris C <chris.c.1221@gmail.com>
2015-08-19 19:18:49 -0400

    update split with exclude

--- app/split_by_chr.sh
+++ app/split_by_chr.sh
@@ -10,4 +10,4 @@
 
 cd /scratch/hpc2862/CAMH/jen/NEAM
 
-/home/hpc2862/Programs/binary_executables/plink --bfile NEAM_QC_complete_150623 --chr $1 --make-bed --out NEAM_QC_complete_150623_chr${1}
+/home/hpc2862/Programs/binary_executables/plink --bfile NEAM_QC_complete_150623 --exclude exclude_snp_list.txt --chr $1 --make-bed --out NEAM_QC_complete_150623_chr${1}
