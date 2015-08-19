59b9c9fb9ef8bd4bee3fcf73e275850aedd925b6
Chris C <chris.c.1221@gmail.com>
2015-08-19 15:37:59 -0400

    commnent and open issue #2: check names of duplicated snps to make sure there aren't more than exm

--- app/remove_dup.R
+++ app/remove_dup.R
@@ -8,20 +8,23 @@ setwd("/scratch/hpc2862/CAMH/jen/NEAM")
 
 neam <- fread("NEAM_QC_complete_150623.bim")
 
-#look forward
+#look forward, getting all the ones which are first
 dup <- neam[base::duplicated(neam$V4),]
 exm_dup <- dup[grep("exm", dup$V2),]
 
-#look backwards
+#look backwards, getting all the dups that are second
 dup <- neam[base::duplicated(neam$V4, fromLast=TRUE),]
 exm_dup2 <- dup[grep("exm", dup$V2),]
 
+#combine together into one vector
 exclude_exm <- c(exm_dup$V2, exm_dup2$V2)
 
+#remove this list form the original data frame
 neam_no_exm <- neam[!(neam$V2 %in% exclude_exm),]
 
+#list the rest of the duplicates, these dont matter which order maybe.  check this
 dup3 <- neam_no_exm[base::duplicated(neam_no_exm$V4),]
 
+#print out and write exclude list to be used with plink
 snp_list <- dup3$V2
-
 write.table("exclude_snp_list.txt", colnames = F, rownames= F, quote = F, sep = " ")
