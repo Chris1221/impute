3ec419519faaa9b1b93d9611111c8aeaf0b72ecf
Chris C <chris.c.1221@gmail.com>
2015-08-19 21:28:25 -0400

    added doc and flip script

---
+++ app/find_flips.sh
@@ -0,0 +1,1 @@
+#!/bin/bashcd /scratch/hpc2862/CAMH/jen/NEAMfor i in $(seq 1 22)do  cat neam_alignment_chr${CHR}.snp.strand | grep "strand" | awk '{ print $2 }' > flip.chr${CHR}cat neam_alignment_chr${CHR}.snp.strand | grep " missing" | awk '{ print $2 }' > exclude.chr${CHR}done
