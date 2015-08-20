192f8f45654675426e3e52bb5c696ca220109d5e
Chris C <chris.c.1221@gmail.com>
2015-08-19 21:48:02 -0400

    EOL characters

--- app/find_flips.sh
+++ app/find_flips.sh
@@ -1,1 +1,12 @@
-#!/bin/bashcd /scratch/hpc2862/CAMH/jen/NEAMfor i in $(seq 1 22)docat neam_alignment_chr${CHR}.snp.strand | grep "strand" | awk '{ print $2 }' > flip.chr${CHR}.txtcat neam_alignment_chr${CHR}.snp.strand | grep " missing" | awk '{ print $2 }' > exclude.chr${CHR}.txtdone
+#!/bin/bash
+
+cd /scratch/hpc2862/CAMH/jen/NEAM
+
+for i in $(seq 1 22)
+do
+
+cat neam_alignment_chr${CHR}.snp.strand | grep "strand" | awk '{ print $2 }' > flip.chr${CHR}.txt
+
+cat neam_alignment_chr${CHR}.snp.strand | grep " missing" | awk '{ print $2 }' > exclude.chr${CHR}.txt
+
+done
