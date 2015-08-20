8d179dead42fd5b3c06577b6135a9cbf99592e8c
Chris C <chris.c.1221@gmail.com>
2015-08-19 22:38:27 -0400

    add phase

---
+++ prephase.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#$ -S /bin/bash
+#$ -q abaqus.q
+#$ -l qname=abaqus.q
+#$ -cwd
+#$ -V
+#$ -j y
+#$ -o /home/hpc2862/logs/$JOB_NAME.txt
+
+cd /home/hpc2862/Scripts/impute
+
+CHR=$1
+REPO="/home/hpc2862/Scripts/impute"
+REF_DIR="/scratch/hpc2862/CAMH/jen/ALL.integrated_phase1_SHAPEIT_16-06-14.nomono"
+DATA_DIR="/scratch/hpc2862/CAMH/jen/NEAM"
+
+bin/shapeit -B ${DATA_DIR}/NEAM_QC_complete_150623_chr${CHR}.flipped -M ${REF_DIR}/genetic_map_chr${CHR}_combined_b37.txt --exclude-snp ${DATA_DIR}/exclude.chr${CHR}.txt --exclude-snp neam_alignment_chr${CHR}.flipped.snp.strand.exclude --input-ref ${REF_DIR}/ALL.chr${CHR}.integrated_phase1_v3.20101123.snps_indels_svs.genotypes.nomono.haplotypes.gz ${REF_DIR}/ALL.chr${CHR}.integrated_phase1_v3.20101123.snps_indels_svs.genotypes.nomono.legend.gz ${REF_DIR}/ALL.integrated_phase1_v3.20101123.snps_indels_svs.genotypes.sample -O ${DATA_DIR}/NEAM_QC_complete_150623_chr${CHR}.flipped.phased
