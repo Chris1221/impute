1e0e3e9e17a048a85444fa474aa15ac597f52a3d
Chris C <chris.c.1221@gmail.com>
2015-12-24 13:05:28 -0500

    start run, have to check

--- app/remove_dup.R
+++ app/remove_dup.R
@@ -1,29 +1,33 @@
 #!/usr/bin/Rscript
-
 require(data.table)
 require(dplyr)
 require(tidyr)
 
-setwd("/scratch/hpc2862/CAMH/jen/NEAM")
+args <- commandArgs(trailingOnly = TRUE)
+DD <- args[1]
+DATA <- args[2]
 
-neam <- fread("NEAM_QC_complete_150623.bim")
+
+setwd(as.character(DD))
+
+df <- fread(paste0(DATA, ".bim"))
 
 #look forward, getting all the ones which are first
-dup <- neam[base::duplicated(neam$V4),]
+dup <- df[base::duplicated(df$V4),]
 exm_dup <- dup[grep("exm", dup$V2),]
 
 #look backwards, getting all the dups that are second
-dup <- neam[base::duplicated(neam$V4, fromLast=TRUE),]
+dup <- df[base::duplicated(df$V4, fromLast=TRUE),]
 exm_dup2 <- dup[grep("exm", dup$V2),]
 
 #combine together into one vector
 exclude_exm <- c(exm_dup$V2, exm_dup2$V2)
 
 #remove this list form the original data frame
-neam_no_exm <- neam[!(neam$V2 %in% exclude_exm),]
+df_no_exm <- df[!(df$V2 %in% exclude_exm),]
 
 #list the rest of the duplicates, these dont matter which order maybe.  check this
-dup3 <- neam_no_exm[base::duplicated(neam_no_exm$V4),]
+dup3 <- df_no_exm[base::duplicated(df_no_exm$V4),]
 
 #print out and write exclude list to be used with plink
 snp_list <- c(exclude_exm, dup3$V2)
