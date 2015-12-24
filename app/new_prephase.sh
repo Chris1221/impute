bf05c5b223e401ea1db70b6b2507f9646e298d57
Christopher Cole <chris.c.1221@gmail.com>
2015-12-24 18:52:31 -0500

    update prephase sub with source

--- app/new_prephase.sh
+++ app/new_prephase.sh
@@ -8,5 +8,7 @@
 #$ -o /home/hpc2862/repos/impute/logs/$JOB_NAME.txt
 
 cd $DD
+source /home/hpc2862/repos/impute/CONFIG
+CHR=$1
 
 $shapeit -B ${DATA}_chr${CHR}.flipped -M ${RD}genetic_map_chr${CHR}_combined_b37.txt --exclude-snp exclude.chr${CHR}.txt -O ${DATA}_chr${CHR}.flipped.phased
