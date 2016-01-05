ff10433b558cedbcbe583d99da185f4f394a737d
Chris C <chris.c.1221@gmail.com>
2016-01-05 12:21:04 -0500

    update run3

--- RUN3.sh
+++ RUN3.sh
@@ -30,7 +30,7 @@ mkdir ../plink
 
 for CHR in `seq 1 22`;
 do
-cat ${DATA}_chr${CHR}.flipped.phased.imputed.* > ../plink/${DATA}_chr${CHR}.imputed.gen
+cat ${DATA}_chr${CHR}.flipped.phased.imputed.* > ${DATA}_chr${CHR}.imputed.gen
 $gtool -G --g ${DATA}_chr${CHR}.imputed.gen --s ../${DATA}_chr${CHR}.flipped.phased.sample --ped ../plink/${DATA}_chr${CHR}.imputed.ped --map ../plink/${DATA}_chr${CHR}.imputed.map --chr ${CHR} --sex sex --threshold 0.9
 
 #remove safeties on pipe
