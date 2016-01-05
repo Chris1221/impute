dea548c9bac9c1d563750e8ae39c7dd9f441eea8
Chris C <chris.c.1221@gmail.com>
2016-01-05 11:19:11 -0500

    remove safeties on pipe and uncomment

--- RUN3.sh
+++ RUN3.sh
@@ -14,25 +14,27 @@ source CONFIG
 
 cd $DD
 
-# mkdir out
-# mkdir process
+mkdir snptest
+mkdir process
 
-# rsync -av --progress ${DATA}_chr*.flipped.phased.imputed.* out/
+rsync -av --progress ${DATA}_chr*.flipped.phased.imputed.* snptest/
 
-# rsync -av --progress ${DATA}_chr*.flipped.phased.imputed.* process/
+rsync -av --progress ${DATA}_chr*.flipped.phased.imputed.* process/
 
 
-cd ${DD}out/
-#mkdir ../plink
-#cp ../*.sample ../plink/
+cd ${DD}snptest/
+mkdir ../plink
+cp ../*.sample ../snptest/
 
-# ls | grep 'info\|summary\|warnings\|diplotype' | xargs -d"\n" rm 
+ls | grep 'info\|summary\|warnings\|diplotype' | xargs -d"\n" rm 
 
 for CHR in `seq 1 22`;
 do
-#cat ${DATA}_chr${CHR}.flipped.phased.imputed.* > ../plink/${DATA}_chr${CHR}.imputed.gen
-# $gtool -G --g ${DATA}_chr${CHR}.imputed.gen --s ../${DATA}_chr${CHR}.flipped.phased.sample --ped ../plink/${DATA}_chr${CHR}.imputed.ped --map ../plink/${DATA}_chr${CHR}.imputed.map --chr ${CHR} --sex sex --threshold 0.9
-$rename ../plink/${DATA}_chr${CHR}.imputed.map ${CHR} > ../plink/${DATA}_chr${CHR}.renamed.imputed.map
+cat ${DATA}_chr${CHR}.flipped.phased.imputed.* > ../plink/${DATA}_chr${CHR}.imputed.gen
+$gtool -G --g ${DATA}_chr${CHR}.imputed.gen --s ../${DATA}_chr${CHR}.flipped.phased.sample --ped ../plink/${DATA}_chr${CHR}.imputed.ped --map ../plink/${DATA}_chr${CHR}.imputed.map --chr ${CHR} --sex sex --threshold 0.9
+
+#remove safeties on pipe
+$rename ../plink/${DATA}_chr${CHR}.imputed.map ${CHR} > ../plink/${DATA}_chr${CHR}.imputed.map
 done
 
 
